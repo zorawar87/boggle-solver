85245be010f13416da10ece2e15e1d29b5ea3ea5
jdrodiger <james@rodiger.net>
2017-12-07 18:01:42 -0500

    Starting to implement search

--- puzzle.c
+++ puzzle.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+void search(char **grid,int nRow,int nCol,char *word);
 
 void main(){
   time_t start, end;
@@ -26,13 +27,18 @@ void main(){
   for(i=0;i<nRow;i++)
     grid[i] = (char *)malloc(nCol*sizeof(char));
 
+  word = (char *)malloc(nRow*sizeof(char));
+  
   for(i=0;i<nRow;i++)
-    scanf("%s",*grid[i]);
+    scanf("%s",grid[i]);
+  
   scanf("%s",word);
   
 
   start = time(NULL);
 
+
+  search(grid,nRow,nCol,word);
   //search for word
 
   end = time(NULL);
@@ -40,3 +46,15 @@ void main(){
   printf("Elapsed time = %ld seconds\n", end - start);
 }
 
+void search(char **grid,int nRow,int nCol,char *word){
+  int i,x,y;
+  for(i=0;word[i+1]!='\0';i++);//find final letter
+  for(y=0;y<nCol;y++){
+    for(x=0;x<nRow;x++){
+      if(grid[y][x]==word[0]){
+	//check right then downleft then down then downright
+      }elseif(grid[y][x]==word[i]){
+      }
+    }
+  }
+}
